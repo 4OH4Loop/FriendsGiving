eeb530583c966ca79828f41aacc44f485f3e646e
4OH4Loop <semperchuck@gmail.com>
2025-03-24 18:30:52 -0400

    Initial Commit

---
+++ FriendsGiving/FriendsGivingApp.swift
@@ -0,0 +1,17 @@
+//
+//  FriendsGivingApp.swift
+//  FriendsGiving
+//
+//  Created by Carolyn Ballinger on 3/24/25.
+//
+
+import SwiftUI
+
+@main
+struct FriendsGivingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
